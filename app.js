972e2a7477bbb89deaeb40fae9166ea57713a020
willissa2121 <you@example.com>
2019-07-08 22:38:21 -0700

    fully functional

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ let mysql = require('mysql')
 let express = require("express");
 let app = express();
 var path = require('path')
-let urlencodedParser = bodyParser.urlencoded({ extended: true })
-
+let urlencodedParser = bodyParser.urlencoded({ extended: true });
+let daddyA = require('./data')
+const PORT = process.env.PORT || 3000;
 
 var connection = mysql.createConnection({
   host: 'localhost',
@@ -22,101 +23,111 @@ var connection = mysql.createConnection({
 connection.connect()
 
 
-app.use(express.static(__dirname + '/public'));
+app.get('/',(req,res)=>{
+  res.sendFile(path.join(__dirname,"./html/index.html"))
+})
 
-const PORT = process.env.PORT || 3100;
-app.get("/", (req, res) => {
-  // res.send("is this even working")
-  res.sendFile(path.join(__dirname, "./html/login.html"));
-});
+app.listen(PORT,()=>{
+  console.log('listening in on port' + PORT)
+})
 
-app.get('/quiz', (req, res) => {
-  res.sendFile(path.join(__dirname, './html/index.html'))
+app.post('/',urlencodedParser,(req,res)=>{
+  getData(req.body)
+  res.redirect('/landing')
 })
 
 
-app.post("/", urlencodedParser, (req, res) => {
-  if (!req.body.Fname || !req.body.Lname) {
-    res.redirect('/')
-  }
-  else {
-
-    let fname = req.body.Fname;
-    let lname = req.body.Lname;
-    let email = req.body.email;
-
-    let query = connection.query(`INSERT INTO users(first_name,last_name,email)VALUES('${fname}','${lname}', '${email}')`, (err, res1) => {
-      if (err) throw err;
-      res.redirect('/quiz')
-
-    })
-  }
-
+app.get('/landing',(req,res)=>{
+  res.send(daddyA)
 })
 
 
-app.listen(PORT, () => {
-  console.log("listening on port" + PORT);
-});
-
-app.post('/quiz', urlencodedParser, (req, res) => {
-
-  let keysA = Object.keys(req.body)
-  let empty = []
-  let num = 0
-  for (var i = 0; i < keysA.length; i++) {
-
-
-    empty.push(Number(req.body[keysA[i]]))
-    num += Number(req.body[keysA[i]])
-
+let getData = x =>{
+  let keysA = Object.keys(x);
+  let empty = [];
+  let sum = 0;
+  for(var i = 0; i < keysA.length; i++){
+    if(keysA[i].charAt(0)==="b"){
+      empty.push(x[keysA[i]])
+    }
   }
-  console.log(empty)
+  for(var i = 0; i < empty.length; i ++){
+    sum+= Number(empty[i]);
+  }
+  let friendSum=compareSum(sum);
+  console.log(friendSum)
+  let matchedIndex=getUserInfo(friendSum)
 
+  let user = new Person(x.Fname,x.Lname,x.email,empty,sum);
+  daddyA.push(user);
 
-  var query = connection.query(
-    `INSERT INTO scores(q1,q2,q3,q4,q5)VALUES(${empty[0]},${empty[1]},${empty[2]},${empty[3]},${empty[4]})`, (err, res1) => {
-      if (err) throw err;
-      res.redirect('/test')
-      createObject()
-    })
+  app.get('/results',(req,res)=>{
+    let fname = daddyA[matchedIndex].firstname;
+    let lname = daddyA[matchedIndex].lastname;
+    let email = daddyA[matchedIndex].email;
 
-})
+    res.sendFile(path.join(__dirname,'./html/result.html'))
 
-
-let createObject = () => {
-
-  
-  let query = connection.query('SELECT id from users', (err, res) => {
-    let empty = []
-    let bigO = []
     
-    if (err) throw err;
-    for (var i = 0; i < res.length; i++) {
-      empty.push(res[i].id)
-    }
-    for(var k = 0; k < empty.length; k++){
-      let query = connection.query(`SELECT * FROM scores INNER JOIN users ON users.id=scores.id WHERE users.id=${empty[k]} `,(err,res1)=>{
-        if(err)throw err;
-        let user = new Account(res1[0].first_name,res1[0].first_name,res1[0].email,)
-        app.get('/test', (req, res) => {
-          res.send(res1)
-        })
-        
-      })
-    }
-
+    
   })
 }
 
-function Account(first, last, email, scores) {
-  this.firstName = first;
-  this.lastName = last;
+
+function Person(fname,lname,email,scores,sum){
+  this.firstname = fname;
+  this.lastname = lname;
   this.email = email;
   this.scores = scores;
+  this.sum = sum;
 }
 
+let compareSum = x =>{
+  let emp = []
+  for(var i = 0; i <daddyA.length;i++){
+    emp.push(daddyA[i].sum)
+  }
+  emp.push(x)
+
+ emp.sort((a,b)=>{return a-b})
+
+ let indexStart = emp.indexOf(x);
+ let lower = indexStart-1;
+ let upper = indexStart+1;
+ if(upper===emp.length){
+   let matchedVal = emp[lower];
+   return matchedVal
+ }
+ else if(lower === -1){
+   let matchedVal = emp[upper];
+   return matchedVal;
+ }
+ else{
+   let lowerV = emp[lower];
+   let upperV = emp[upper];
+   let lowerA = x-lowerV;
+   let upperA = Math.abs(x-upperV)
+
+   if(upperA >= lowerA){
+    let matchedVal = lowerV
+    return matchedVal;
+   }
+   else{
+     let matchedVal = upperV
+     return matchedVal;
+   }
+   
+ }
 
 
+}
 
+let getUserInfo = (x)=>{
+  for(var i = 0; i< daddyA.length; i++){
+    if(daddyA[i].sum === x){
+      let t = i;
+      return t;
+    }
+  }
+}
 
---
+++ data.js
@@ -0,0 +1,35 @@
+let daddyA = [
+  {
+    firstname : 'mark',
+    lastname : "gillam",
+    email : 'mark.gillam@example.com',
+    scores : [1,1,1,1,1],
+    sum : 5
+  }
+  ,
+  {
+    firstname : 'matt',
+    lastname : "verde",
+    email : 'matt.verde@example.com',
+    scores : [3,3,3,3,3],
+    sum : 15
+  }
+  ,
+  {
+    firstname : 'luke',
+    lastname : "league",
+    email : 'luke.league@example.com',
+    scores : [5,5,5,5,5],
+    sum : 25
+  }
+  ,
+  {
+    firstname : 'carie',
+    lastname : "graves",
+    email : 'carie.graves@example.com',
+    scores : [2,2,2,2,2],
+    sum : 10
+  }
+]
+
+module.exports = daddyA;
