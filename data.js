89cb9e62b3b05d9aa28a7457ec2d359e9e65c644
Scott Willis <scott.willis.wsu@gmail.com>
2019-07-09 11:09:02 -0700

    addq

--- data.js
+++ data.js
@@ -1,34 +1,34 @@
 let daddyA = [
   {
-    firstname : 'mark',
-    lastname : "gillam",
-    email : 'mark.gillam@example.com',
-    scores : [1,1,1,1,1],
-    sum : 5
+    firstname: 'mark',
+    lastname: "gillam",
+    email: 'mark.gillam@example.com',
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    sum: 10
   }
   ,
   {
-    firstname : 'matt',
-    lastname : "verde",
-    email : 'matt.verde@example.com',
-    scores : [3,3,3,3,3],
-    sum : 15
+    firstname: 'matt',
+    lastname: "verde",
+    email: 'matt.verde@example.com',
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    sum: 30
   }
   ,
   {
-    firstname : 'luke',
-    lastname : "league",
-    email : 'luke.league@example.com',
-    scores : [5,5,5,5,5],
-    sum : 25
+    firstname: 'luke',
+    lastname: "league",
+    email: 'luke.league@example.com',
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+    sum: 50
   }
   ,
   {
-    firstname : 'carie',
-    lastname : "graves",
-    email : 'carie.graves@example.com',
-    scores : [2,2,2,2,2],
-    sum : 10
+    firstname: 'carie',
+    lastname: "graves",
+    email: 'carie.graves@example.com',
+    scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    sum: 20
   }
 ]
 
